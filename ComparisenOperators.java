9940de60b91d01b319d58c04d802de91141c8882
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 14:29:42 +0530

    op

---
+++ ComparisenOperators.java
@@ -0,0 +1,15 @@
+public class ComparisenOperators {
+    public static void main(String[] args) {
+		
+		int a =6;
+		int b =6;
+
+		System.out.println(a>b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+
+	}
+}
