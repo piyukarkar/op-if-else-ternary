0a70dcb975f6842c6f5b872102beb5c3a59a84f9
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 14:43:19 +0530

    op

---
+++ Condition/Ternary.java
@@ -0,0 +1,14 @@
+package Condition;
+
+public class Ternary {
+    public static void main(String[] args) {
+		
+		int a = 15;
+		int b = 8;
+		
+		int maxOfBothNumbers = 0;
+		
+		maxOfBothNumbers = a > b ? a : b;
+                System.out.println("max of both number is" + maxOfBothNumbers);
+	}
+}
