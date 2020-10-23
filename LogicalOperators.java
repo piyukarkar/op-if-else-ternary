694c59c4374bae330c35e008c90fb5ffc43c6cb3
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 14:31:21 +0530

    op

---
+++ LogicalOperators.java
@@ -0,0 +1,26 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+		
+		int number =-4;
+
+		if(number <=-1  && number >=-10) {
+			System.out.println("Number is in the range");
+		
+		int grade =10;
+		
+		if(grade ==10 || grade ==12) {
+			
+			System.out.println("you can give board exam now");
+		
+		
+		
+                int grade =11;
+		
+		if(!(grade ==10 || grade ==12)) {
+			
+			System.out.println("you can not give board exam now");
+				 }
+
+	}
+
+}
