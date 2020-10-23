b12ac393f2da83d110157d332e11ebf38d598515
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 14:17:29 +0530

    op

---
+++ ArithmeticOperators.java
@@ -0,0 +1,31 @@
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		
+		int a = 23;
+		int b = 45;
+		
+		int c= a * a + b * b + 2 * a * b;
+		
+		int d = (a+b) *(a+b);
+
+		System.out.println(c +" "+ d);
+
+          	int c=b/a;
+
+		System.out.println(c );
+
+          	double c = (double)b/ (double)a;
+
+		System.out.println(c );
+		
+		int c =b++ ;
+		
+		int  c =b--;
+		
+		System.out.println(c +" "+ b);
+		
+		
+	}
+
+}
