227adcc4031d1ec9316fad952d27a2b650cde4e5
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 14:27:55 +0530

    op

---
+++ BitwiseOperators.java
@@ -0,0 +1,15 @@
+public class BitwiseOperators {
+    public static void main(String[] args) {
+		
+	    int a = 10;
+		
+	    int b = 13;
+	    
+	    int c = a | b;
+	    System.out.println(c);
+
+	    int c = b <<2;
+	    System.out.println(c);
+
+	}
+}
